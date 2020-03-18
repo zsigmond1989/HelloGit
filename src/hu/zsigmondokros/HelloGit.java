078977db405a9da22085da3b4dfe31cd129fe352
Okros Zsigmond <zsigmond1989@gmail.com>
2020-03-18 13:04:50 +0100

    Add second sout

--- src/hu/zsigmondokros/HelloGit.java
+++ src/hu/zsigmondokros/HelloGit.java
@@ -3,6 +3,7 @@ package hu.zsigmondokros;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello git!");
+        System.out.println("még egy sor ");
 
     }
 }
